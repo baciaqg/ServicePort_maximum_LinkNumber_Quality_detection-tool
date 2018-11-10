ccd0b48c84d762283da0df64ad1a264797ab675d
pay748 <40951645+pay748@users.noreply.github.com>
2018-11-10 11:07:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ServicePort_maximum_LinkNumber_Quality_detection-tool
+
+功能：
+
+    1.应用层服务端口连通性测试。
+    2.链路丢包率检测，链路质量检测
+    3.服务并发最大会话数侦测，服务tcp连接性能测试。
---
+++ socket_testing(v2.0).py
@@ -0,0 +1,58 @@
+#encoding=utf-8
+import socket,time
+from threading import *
+
+ip=str(input('输入测试服务的IP地址:'))
+port=int(input('输入该服务使用的业务端口号:'))
+num=int(input('输入每次测试发起TCP连接数：'))
+# ip='8.8.8.8'
+# port=53
+# num=50
+
+
+s1=Semaphore(value=num)
+s2=Semaphore(value=1)
+class test():
+    m=0
+    def __init__(self,ip,port,ii):
+        self.ip=ip
+        self.port=port
+        self.ii=ii
+    def Port_connect(self):
+        s1.acquire()
+        sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # sk.settimeout(0.05)
+        try:
+            # print(self.ip,self.port)
+            sk.connect((self.ip,self.port))
+            # print('连接成功',time.localtime(time.time()))
+            self.m+=1
+        except Exception as e:
+            s2.acquire()
+            print('第'+str(ii)+'次测试连接失败一次')
+            # print(e)
+            s2.release()
+        # time.sleep(1)
+        s1.release()
+        sk.close()
+        # print(self.m)
+        # time.sleep(0.2)
+
+
+# a.Port_connect()
+for ii in range(10000):
+    a=test(ip,port,ii)
+    for i in range(num):
+        t1=Thread(target=a.Port_connect,args=())  #target=后面的函数不要瞎几把乱加括号，影响并发
+        t1.start()
+    # time.sleep(5)
+    # t1.join()
+    while t1.is_alive()==True:  #子线程运行状态判断
+        # pass
+        # print('*',end="")
+        time.sleep(15)
+
+    # print(t1.is_alive())
+    print (ip+':'+str(port)+' 第'+str(ii+1)+'测试'+str(num)+'包丢包率为: {:.2%}'.format((num-a.m)/num), a.m)
+    a.m=0
+
